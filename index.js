7c5de1d4c9cbf01ddd790b9c62f616990fe2aa65
Falcon <mail@sokolvlad.ru>
2022-03-29 08:57:46 +0300

    https://youtu.be/ne97G3DlBwg?list=PL9Gk1O6XmsobWOSyZEMD4_Nz-H_10gXHo&t=656

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
 
-console.log('test')
+//Переменные
+
+const firstName = "Vlad"
+const LastName = "Sokol"
+let age = 46
+
+console.log(firstName, LastName, `${age} годиков`)
+
+//Операторы
+// const flash = prompt("Your name?")
+// alert(`My name: ${flash}` )
+
+console.log(`My name: ${firstName}, age: ${age}`)
+
+const y = 5
+const x = 15
+
+console.log(y-x)
+console.log(y+x)
+console.log(y/x)
+console.log(y*x)
+
+
+let k, n, r, oper
+
+k = prompt('Type first number')
+n = prompt('Type seond number')
+oper = prompt('Type an operator')
+
+if (oper == '+') {
+    r = Number(k) + Number(n)
+}
+if (oper == '-') {
+    r = Number(k) - Number(n)
+}
+if (oper == '/') {
+    r = Number(k) / Number(n)
+}
+if (oper == '*') {
+    r = Number(k) * Number(n)
+}
+
+alert(`You result is: ${r}`)
