868f5c627ddfaf29f3a0fe8b1c9e09754161a1e9
Falcon <mail@sokolvlad.ru>
2022-03-31 10:34:37 +0300

    https://youtu.be/Wm5s87b5K3M?list=PL9Gk1O6XmsobWOSyZEMD4_Nz-H_10gXHo&t=2

--- index.js
+++ index.js
@@ -7,3 +7,29 @@ console.log(arr);
 for (let i of arr) {
   console.log(i);
 }
+
+var myHonda = {
+  color: "red",
+  wheels: 4,
+  engine: {
+    cylinders: 4,
+    size: 2.2,
+  },
+};
+
+console.log(myHonda);
+
+const int = 10;
+const str = "15";
+console.log(Number(str) + int);
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+console.log(Math.ceil(getRandomArbitrary(100000, 999999)));
+
+for (const i in [...Array(10).keys()]) {
+  r = Math.ceil(getRandomArbitrary(100000, 999999));
+  console.log(i + " => " + r);
+}
